930a5bf23ce25d15fbfa36164c57da124da79cf2
matwu <matwumatwu@gmail.com>
2016-03-15 08:58:03 +0000

    modified database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20160314102441) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",      limit: 255
+    t.string   "content",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
